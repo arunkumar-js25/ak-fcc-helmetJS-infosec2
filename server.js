d0cc8cee0bddfc82508a6ec789d4b5d28402160c
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-22 15:10:44 +0000

    14/14 infosec Part 2 Practise lessons

--- server.js
+++ server.js
@@ -11,15 +11,23 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  console.log(hash);
+  
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    /*res == true or false*/
+    console.log(res); //true
+  });
 
+});
 
 
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 //END_SYNC
 
 
